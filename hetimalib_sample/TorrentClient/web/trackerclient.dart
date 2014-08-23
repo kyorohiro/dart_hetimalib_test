c6e4b808a698a2140e9d89dc2bdb30bfeb6bd043
kyorohiro <kyorohiro@gmail.com>
2014-08-23 18:30:42 +0900

    ..

--- hetimalib_sample/TorrentClient/web/trackerclient.dart
+++ hetimalib_sample/TorrentClient/web/trackerclient.dart
@@ -5,55 +5,58 @@ import 'package:hetima/hetima.dart' as hetima;
 import 'package:hetima/hetima_cl.dart' as hetimacl;
 
 hetima.TrackerClient client = new hetima.TrackerClient(new hetimacl.HetiSocketBuilderChrome());
-
+hetima.TorrentFile torrentFile = null;
+html.LabelElement memoField = null;
 void main() {
   
   html.Element startServerButton = new html.Element.html('<input id="startServerButton" type="button" value="startServer"> ');
   html.InputElement fileSelector = new html.Element.html("""<input type="file" id="files" name="file" />""");
-  html.LabelElement memoField = new html.LabelElement();
-
+  html.Element requestTrackerButton = new html.Element.html('<input id="requestTrackerButton" type="button" value="requestTracker"> ');
+  memoField = new html.LabelElement();
   html.document.body.append(new html.Element.html("<div>### torrent file</div>"));
   html.document.body.append(fileSelector);
   html.document.body.append(new html.Element.html("<div>### </div>"));
   html.document.body.append(startServerButton);
+  html.document.body.append(new html.Element.html("<div>### </div>"));
+  html.document.body.append(requestTrackerButton);
   html.document.body.append(new html.Element.html("<div>### </div>"));
   html.document.body.append(memoField);
 
   startServerButton.onClick.listen((html.MouseEvent e){
     startServer();
   });
+  requestTrackerButton.onClick.listen((html.MouseEvent e) {
+    if(torrentFile == null) {
+      return;
+    }
+    requestTracker(torrentFile);
+  });
+
   fileSelector.onChange.listen((html.Event e) {
     memoField.innerHtml = "";
     html.File file = null;   
-    for (html.File f in fileSelector.files) {
-      file =f;
-      print("==" + f.name);
-      memoField.appendText("==" + f.name);
-      memoField.appendHtml("<br>");
-      break;
+    if(fileSelector.files.length < 1){
+      return;
     }
-    if(file == null) {return;}
-    
-    hetimacl.HetimaFileBlob hetimaFile = new hetimacl.HetimaFileBlob(file);
-    hetima.HetimaBuilder builder = new hetima.HetimaFileToBuilder(hetimaFile);
-    hetima.HetiBencode.decode(new hetima.EasyParser(builder)).then((Object o) {
-      if(o is Map) {
-        Map dict = o;
-        for(Object k in dict.keys) {
-          if(dict[k] != null) {
-            print("="+dict[k].toString());
-            memoField.appendText("#"+k.toString()+ "=" + dict[k].toString()+"");
-            memoField.appendHtml("<br>");
-          }
-        }
-      }
-    }).catchError((e) {
-      print("= parsr error");
-      memoField.appendText("= parser error");
-    });
+    startDecodeTorrentFile(fileSelector.files.last);
   });
 
-  
+}
+
+void startDecodeTorrentFile(html.File file) {
+  memoField.appendText("==" + file.name);
+  memoField.appendHtml("<br>");
+
+  hetimacl.HetimaFileBlob hetimaFile = new hetimacl.HetimaFileBlob(file);
+  hetima.HetimaBuilder builder = new hetima.HetimaFileToBuilder(hetimaFile);
+  hetima.TorrentFile.createTorrentFileFromTorrentFile(builder).then((hetima.TorrentFile f) {
+    memoField.appendText("#"+f.mMetadata.toString());
+    memoField.appendHtml("<br>");
+    
+  }).catchError((e) {
+    print("= parsr error");
+    memoField.appendText("= parser error");
+  });
 }
 
 void startClient() {
@@ -61,7 +64,7 @@ void startClient() {
 }
 
 void startServer() {
-  print("resizeWindow");
+  print("start server");
   hetimacl.HetiServerSocketChrome
   .startServer("127.0.0.1", 8088)
   .then((hetima.HetiServerSocket socket){
@@ -76,5 +79,14 @@ void startServer() {
       });
     });
   });
-  print("##resizeWindow");
+}
+
+void requestTracker(hetima.TorrentFile file) {
+  print("request tracker");
+  hetima.TrackerClient client = new hetima.TrackerClient(new hetimacl.HetiSocketBuilderChrome());
+  client.event = hetima.TrackerUrl.VALUE_EVENT_STARTED;
+  client.updateFromMetaData(file).then((v){
+    return client.request();
+  }).then((hetima.TrackerRequestResult req) {
+  });
 }
