0754b3825e5d0b777f2d1c77d0aed7a6c4945430
kyorohiro <kyorohiro@gmail.com>
2014-09-06 22:38:15 +0900

    ..

--- hetimalib_sample/SSDPClient/web/ssdpclient.dart
+++ hetimalib_sample/SSDPClient/web/ssdpclient.dart
@@ -9,30 +9,35 @@ hetima.UpnpDeviceSearcher ssdp = new hetima.UpnpDeviceSearcher(new hetimacl.Heti
 
 html.LabelElement deviceInfoMemo = null;
 html.LabelElement addressInfoMemo = null;
+html.LabelElement localAddressInfoMemo = null;
 void main() {
-  html.Element requestDiscover = new html.Element.html('<input id="requestDiscoverButton" type="button" value="requestDiscover"> ');
-  html.Element bindjoinNetwork = new html.Element.html('<input id="bindjoinNetworkButton" type="button" value="bindjoinNetwork"> ');
-  html.Element getServiceButton = new html.Element.html('<input id="requestMyIPButton" type="button" value="requestMyIP"> ');
-  html.Element testButton = new html.Element.html('<input id="testButton" type="button" value="testButton"> ');
+  html.Element bindjoinNetwork = new html.Element.html('<input id="bindjoinNetworkButton" type="button" value="[1]bindjoinNetwork"> ');
+  html.Element requestDiscover = new html.Element.html('<input id="requestDiscoverButton" type="button" value="[2]requestDiscover"> ');
+  html.Element getServiceButton = new html.Element.html('<input id="requestMyIPButton" type="button" value="[3]requestMyIP"> ');
+  html.Element getLocalAddressButton = new html.Element.html('<input id="testButton" type="button" value="[4]getLocalAddress"> ');
 
   deviceInfoMemo = new html.LabelElement();
   addressInfoMemo = new html.LabelElement();
+  localAddressInfoMemo = new html.LabelElement();
 
   html.document.body.append(new html.Element.html("<div>### </div>"));
   html.document.body.append(bindjoinNetwork);
 
   html.document.body.append(new html.Element.html("<div>### </div>"));
   html.document.body.append(requestDiscover);
-
+  html.document.body.append(new html.Element.html("<div>### </div>"));
+  html.document.body.append(deviceInfoMemo);
   html.document.body.append(new html.Element.html("<div>### </div>"));
   html.document.body.append(getServiceButton);
 
   html.document.body.append(new html.Element.html("<div>### </div>"));
-  html.document.body.append(deviceInfoMemo);
-
-  html.document.body.append(new html.Element.html("<div>### </div>"));
   html.document.body.append(addressInfoMemo);
-  html.document.body.append(testButton);
+  html.document.body.append(new html.Element.html("<div>### </div>"));
+
+  html.document.body.append(getLocalAddressButton);
+  html.document.body.append(new html.Element.html("<div>### </div>"));
+  html.document.body.append(localAddressInfoMemo);
+  html.document.body.append(new html.Element.html("<div>### </div>"));
 
   ssdp.onReceive().listen((hetima.UPnpDeviceInfo info) {
     deviceInfoMemo.text = "device:"+info.getValue(hetima.UPnpDeviceInfo.KEY_LOCATION, "not found");
@@ -53,12 +58,18 @@ void main() {
     }
   });
 
-  testButton.onClick.listen((html.MouseEvent e) {
+  getLocalAddressButton.onClick.listen((html.MouseEvent e) {
     chrome.system.network.getNetworkInterfaces().then((List<chrome.NetworkInterface> nl){
       for(chrome.NetworkInterface i in nl) {
         print("address:"+i.address);
         print("name:"+i.name);
         print("name:"+i.prefixLength.toString());
+      }
+      for(chrome.NetworkInterface i in nl) {
+      if(i.prefixLength == 24) {
+        localAddressInfoMemo.text = i.address;
+        break;
+      }
       }
     });
     DummyServer server = new DummyServer();
